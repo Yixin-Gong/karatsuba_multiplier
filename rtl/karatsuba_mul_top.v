4d4a773ac1ef633909a33838ef0a0d3b71ca9184
CMOS-Lin <128216436+CMOS-Lin@users.noreply.github.com>
2024-01-03 19:54:03 +0800

    Update karatsuba_mul_top.v

--- rtl/karatsuba_mul_top.v
+++ rtl/karatsuba_mul_top.v
@@ -1,14 +1,31 @@
 module karatsuba_mul_top(
+	input		clk,
+	input		rst_n,
 	input 	    [31:0] A,
 	input 	    [31:0] B,
 	output	    [63:0] C
 );
 
+reg [31:0] a,b;
+
 karatsuba_mul_32 dut(
-	.A			(A),
-    .B			(B),
+	.A			(a),
+    .B			(b),
     .C			(C)
 );
+
+always @ (posedge clk or negedge rst_n)
+begin
+	if(!rst_n) begin
+	a <= 32'b00000000_00000000_00000000_00000000;
+	b <= 32'b00000000_00000000_00000000_00000000;
+	end
+	else begin
+	a <= A;
+	b <= B;
+	end	
+end
+
 
 endmodule
 
