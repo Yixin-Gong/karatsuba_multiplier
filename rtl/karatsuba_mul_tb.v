905f64ea4547e1eb6171211a0d5252b7b8e0075e
CMOS-Lin <128216436+CMOS-Lin@users.noreply.github.com>
2024-01-03 19:54:53 +0800

    Update karatsuba_mul_tb.v

--- rtl/karatsuba_mul_tb.v
+++ rtl/karatsuba_mul_tb.v
@@ -6,14 +6,23 @@ parameter n=64;
 reg [m-1:0] A;
 reg [m-1:0] B;
 wire [n-1:0] C;
+reg clk,rst_n;
 
 karatsuba_mul_top dut(
+	.clk		(clk),
+	.rst_n		(rst_n),
 	.A			(A),
     .B			(B),
     .C			(C)
 );
 
 initial begin
+	clk <=1'b0;
+	forever #60 clk <= ~clk;
+end
+
+initial begin
+	rst_n <=1'b1;
 	 #50
     B = 32'b10111011_11011011_10000011_11011011;
     A = 32'b11011011_10010011_10111011_11011011;
